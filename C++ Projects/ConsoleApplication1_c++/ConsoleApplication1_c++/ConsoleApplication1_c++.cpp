c2cf52ad0c2c860e38369aaaf8c311789a795e0d
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-04-26 16:23:19 +0300

    Move codes to main.cpp

--- C++ Projects/ConsoleApplication1_c++/ConsoleApplication1_c++/ConsoleApplication1_c++.cpp
+++ C++ Projects/ConsoleApplication1_c++/ConsoleApplication1_c++/ConsoleApplication1_c++.cpp
@@ -1,32 +1,4 @@
 // ConsoleApplication1_c++.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-using namespace std;
 
-struct Worker{
-    int number;
-    string name;
-    string surname;
-    string department;
-    char gender;
-};
-
-
-void changeName(Worker* renamedWorker, string newName) {
-    renamedWorker->name = newName;
-}
-
-int main()
-{
-    Worker worker1;
-
-    worker1.name = "Ozge";
-    worker1.surname = "Durgut";
-    worker1.department = "Software";
-
-    cout << worker1.name << endl;
-    changeName(&worker1, "Hulya");
-    cout << worker1.name << endl;
-    return 0;
-}
